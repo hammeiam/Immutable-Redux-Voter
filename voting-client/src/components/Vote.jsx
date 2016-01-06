9b8805b067b5872150900909cc4aa8c4bbd2f640
David Hamme <dhamme@gmail.com>
2016-01-06 00:15:03 -0800

    split Voting into seperate components

--- voting-client/src/components/Vote.jsx
+++ voting-client/src/components/Vote.jsx
@@ -7,22 +7,20 @@ export default React.createClass({
   isDisabled: function() {
     return !!this.props.hasVoted;            
   },
-  hasVotedFor: function(entry){
+  hasVotedFor: function(entry) {
     return this.props.hasVoted === entry;             
   },
   render: function() {
-    return <div className='voting'>
-      {this.props.winner ? 
-        <div ref="winner">Winner is {this.props.winner}!</div> :
-        this.getPair().map(entry =>
-          <button key={entry}
+    return <div className="voting">
+      {this.getPair().map(entry =>
+        <button key={entry}
                 disabled={this.isDisabled()}
                 onClick={() => this.props.vote(entry)}>
-            <h1>{entry}</h1>
-            {this.hasVotedFor(entry) ?
-              <div className="label">Voted</div> :
-              null}
-          </button>
+        <h1>{entry}</h1>
+        {this.hasVotedFor(entry) ? 
+          <div className="label">Voted</div> :
+          null }
+        </button>
       )}
     </div>;
   }
