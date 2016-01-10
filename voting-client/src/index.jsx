c2c3a21d7ef55597157da19b9043106e468850de
David Hamme <dhamme@gmail.com>
2016-01-09 19:53:14 -0800

    bug fix to backend, add more socket.io to client

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -4,21 +4,18 @@ import Router, {Route} from 'react-router';
 import {createStore} from 'redux';
 import reducer from './reducer';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import Voting from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state => 
+  store.dispatch({type: 'SET_STATE', state})    
+);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
